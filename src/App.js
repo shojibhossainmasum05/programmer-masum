2a1a7adcd39cc198c143da483903901469e418cb
masumbillha05 <maisaraislam19@gmail.com>
2020-05-20 16:41:00 +0600

    44 Number line Problams

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.Config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+  const {user, setUser} = useState
+  ({
+        isSignedIn: false,
+        name : '',
+        email : '',
+        photo : ''
+  });
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+    const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+    .then(result => {
+    const {photoURL, displayName, email} = result;
+
+  const signedInUser =
+  {
+        isSignedIn : true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+  }
+
+  setUser(signedInUser);
+  })
+  
+      .catch(error => {
+      console.log(error);
+      console.log(error.message);
+  })
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={handleSignIn}> Sign In </button>
+    {
+        user.isSignedIn && 
+      <div>
+        <p> welcome, {user.name} </p>
+        <p> Your E-mail :  {user.email} </p>
+        <img src = {user.photo} alt=""> </img>
+      </div>
+    }
     </div>
   );
 }
---
+++ src/firebase.Config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJXseAYot9Zh4j73DEiDCG3Af0eSNP1IM",
+    authDomain: "programmer-masm.firebaseapp.com",
+    databaseURL: "https://programmer-masm.firebaseio.com",
+    projectId: "programmer-masm",
+    storageBucket: "programmer-masm.appspot.com",
+    messagingSenderId: "1006207322416",
+    appId: "1:1006207322416:web:530ef4d71375d5ff2460f5",
+    measurementId: "G-LM4S79ED18"
+  };
+
+
+
+  export default firebaseConfig
